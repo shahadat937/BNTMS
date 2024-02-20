698c5cbe74ed67c1889e2bb620866fc87ede85cc
Shahadat <shahadathossan937@gmail.com>
2024-02-20 18:31:54 +0600

    Update code add on CourseWeekAllController.cs

---
+++ SchoolManagement.Application/Features/CourseWeekAll/Requests/Commands/UpdateCourseWeekAllCommand.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using SchoolManagement.Application.DTOs.CourseWeekAll;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolManagement.Application.Features.CourseWeekAll.Requests.Commands
+{
+    public class UpdateCourseWeekAllCommand:IRequest<Unit>
+    {
+        public CourseWeekAllDto CourseWeekAllDto { get; set; }
+    }
+}
