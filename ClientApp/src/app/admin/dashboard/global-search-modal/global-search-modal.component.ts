55c01a96c2ef4777ae06718f855ae6f0139bafae
Joy <joymahanta9@infinitytechltd.com>
2024-09-28 17:53:49 +0600

    Added search button icon in header, created global search modal component

---
+++ ClientApp/src/app/admin/dashboard/global-search-modal/global-search-modal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-global-search-modal',
+  templateUrl: './global-search-modal.component.html',
+  styleUrls: ['./global-search-modal.component.sass']
+})
+export class GlobalSearchModalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
