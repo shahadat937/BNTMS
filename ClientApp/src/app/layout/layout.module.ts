1fd723797ce63dd6730213cd5a5baf13c3a4ee00
Joy <joymahanta9@infinitytechltd.com>
2024-09-29 17:52:02 +0600

    Imported necessary package in app module and layout module

--- ClientApp/src/app/layout/layout.module.ts
+++ ClientApp/src/app/layout/layout.module.ts
@@ -6,8 +6,11 @@ import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import { AuthLayoutComponent } from './app-layout/auth-layout/auth-layout.component';
 import { MainLayoutComponent } from './app-layout/main-layout/main-layout.component';
+import {MatDialog, MatDialogModule} from '@angular/material/dialog';
+import {GlobalSearchModalComponent} from '../admin/dashboard/global-search-modal/global-search-modal.component'
 @NgModule({
-  imports: [CommonModule, NgbModule, MatTabsModule, MatButtonModule,MatIconModule],
+  imports: [CommonModule, NgbModule, MatTabsModule, MatButtonModule,MatIconModule, MatDialog, MatDialogModule],
   declarations: [AuthLayoutComponent, MainLayoutComponent],
+  entryComponents: [GlobalSearchModalComponent]
 })
 export class LayoutModule {}
