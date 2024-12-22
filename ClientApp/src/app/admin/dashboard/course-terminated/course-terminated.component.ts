f5644d71fe1d25121943ee1f269be8cfe4e6aad0
infinityaziz <abdulaziz@infinitytechltd.com>
2024-12-22 10:58:58 +0600

    course terminated component created

---
+++ ClientApp/src/app/admin/dashboard/course-terminated/course-terminated.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-course-terminated',
+  templateUrl: './course-terminated.component.html',
+  styleUrls: ['./course-terminated.component.sass']
+})
+export class CourseTerminatedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
