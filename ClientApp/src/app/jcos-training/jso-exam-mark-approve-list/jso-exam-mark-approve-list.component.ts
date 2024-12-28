84eff57b5776b7b89732a801fad313b13109241b
infinityaziz <abdulaziz@infinitytechltd.com>
2024-12-28 15:40:46 +0600

    add jso exam mark approve component

---
+++ ClientApp/src/app/jcos-training/jso-exam-mark-approve-list/jso-exam-mark-approve-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-jso-exam-mark-approve-list',
+  templateUrl: './jso-exam-mark-approve-list.component.html',
+  styleUrls: ['./jso-exam-mark-approve-list.component.sass']
+})
+export class JsoExamMarkApproveListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
