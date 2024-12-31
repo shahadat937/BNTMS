4feaba1f2173533b2162119624912fe3425c3b51
shahadat <shahadathossan937@gmail.com>
2024-12-31 13:43:15 +0600

    code beautity on ForeignCourseOtherDocumentComponent&ForeignCourseGOInfoService&ForeignCourseOtherDocService&ForeignCourseOtherDocumentService

--- ClientApp/src/app/air-ticket/foreigncourseotherdocument/foreigncourseotherdocument-list/foreigncourseotherdocument-list.component.ts
+++ ClientApp/src/app/air-ticket/foreigncourseotherdocument/foreigncourseotherdocument-list/foreigncourseotherdocument-list.component.ts
@@ -1,26 +1,26 @@
-import { SelectionModel } from '@angular/cdk/collections';
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-import { MatPaginator, PageEvent } from '@angular/material/paginator';
-import { MatTableDataSource } from '@angular/material/table';
-import { ForeignCourseOtherDocument } from '../../models/ForeignCourseOtherDocument';
-import { ForeignCourseOtherDocumentService } from '../../service/ForeignCourseOtherDocument.service';
-import { ActivatedRoute, Router } from '@angular/router';
-import { MatSnackBar } from '@angular/material/snack-bar';
-import { MasterData } from '../../../../assets/data/master-data';
-import { ConfirmService } from '../../../core/service/confirm.service';
-import { SharedServiceService } from '../../../shared/shared-service.service';
-import { UnsubscribeOnDestroyAdapter } from '../../../shared/UnsubscribeOnDestroyAdapter';
-
-
+import { SelectionModel } from "@angular/cdk/collections";
+import { Component, ElementRef, OnInit, ViewChild } from "@angular/core";
+import { MatPaginator, PageEvent } from "@angular/material/paginator";
+import { MatTableDataSource } from "@angular/material/table";
+import { ForeignCourseOtherDocument } from "../../models/ForeignCourseOtherDocument";
+import { ForeignCourseOtherDocumentService } from "../../service/ForeignCourseOtherDocument.service";
+import { ActivatedRoute, Router } from "@angular/router";
+import { MatSnackBar } from "@angular/material/snack-bar";
+import { MasterData } from "../../../../assets/data/master-data";
+import { ConfirmService } from "../../../core/service/confirm.service";
+import { SharedServiceService } from "../../../shared/shared-service.service";
+import { UnsubscribeOnDestroyAdapter } from "../../../shared/UnsubscribeOnDestroyAdapter";
 
 @Component({
-  selector: 'app-foreigncourseotherdocumentcomponent-list',
-  templateUrl: './foreigncourseotherdocument-list.component.html',
-  styleUrls: ['./foreigncourseotherdocument-list.component.sass']
+  selector: "app-foreigncourseotherdocumentcomponent-list",
+  templateUrl: "./foreigncourseotherdocument-list.component.html",
+  styleUrls: ["./foreigncourseotherdocument-list.component.sass"],
 })
-export class ForeignCourseOtherDocumentComponent extends UnsubscribeOnDestroyAdapter implements OnInit {
-
-   masterData = MasterData;
+export class ForeignCourseOtherDocumentComponent
+  extends UnsubscribeOnDestroyAdapter
+  implements OnInit
+{
+  masterData = MasterData;
   loading = false;
   ELEMENT_DATA: ForeignCourseOtherDocument[] = [];
   isLoading = false;
@@ -28,37 +28,45 @@ export class ForeignCourseOtherDocumentComponent extends UnsubscribeOnDestroyAda
   paging = {
     pageIndex: this.masterData.paging.pageIndex,
     pageSize: this.masterData.paging.pageSize,
-    length: 1
-  }
-  searchText="";
+    length: 1,
+  };
+  searchText = "";
 
-  displayedColumns: string[] = [ 'sl','courseDurationId', 'courseNameId',   'foreignCourseDocTypeId', 'status', 'fileUpload',   'actions'];
-  dataSource: MatTableDataSource<ForeignCourseOtherDocument> = new MatTableDataSource();
+  displayedColumns: string[] = [
+    "sl",
+    "courseDurationId",
+    "courseNameId",
+    "foreignCourseDocTypeId",
+    "status",
+    "fileUpload",
+    "actions",
+  ];
+  dataSource: MatTableDataSource<ForeignCourseOtherDocument> =
+    new MatTableDataSource();
 
   selection = new SelectionModel<ForeignCourseOtherDocument>(true, []);
 
-  
   constructor(
     private route: ActivatedRoute,
     private snackBar: MatSnackBar,
     private ForeignCourseOtherDocumentService: ForeignCourseOtherDocumentService,
     private router: Router,
     private confirmService: ConfirmService,
-    public sharedService: SharedServiceService,) {
+    public sharedService: SharedServiceService
+  ) {
     super();
   }
-  
+
   ngOnInit() {
-   // this.getForeignCourseOtherDocuments();
+    // this.getForeignCourseOtherDocuments();
   }
-  
+
   // getForeignCourseOtherDocuments() {
   //   this.isLoading = true;
   //   this.ForeignCourseOtherDocumentService.getForeignCourseOtherDocuments(this.paging.pageIndex, this.paging.pageSize,this.searchText).subscribe(response => {
-     
 
-  //     this.dataSource.data = response.items; 
-  //     this.paging.length = response.totalItemsCount    
+  //     this.dataSource.data = response.items;
+  //     this.paging.length = response.totalItemsCount
   //     this.isLoading = false;
   //   })
   // }
@@ -75,38 +83,36 @@ export class ForeignCourseOtherDocumentComponent extends UnsubscribeOnDestroyAda
           this.selection.select(row)
         );
   }
-  addNew(){
-    
-  }
- 
+  addNew() {}
+
   pageChanged(event: PageEvent) {
-    this.paging.pageIndex = event.pageIndex
-    this.paging.pageSize = event.pageSize
-    this.paging.pageIndex = this.paging.pageIndex + 1
-   // this.getForeignCourseOtherDocuments();
+    this.paging.pageIndex = event.pageIndex;
+    this.paging.pageSize = event.pageSize;
+    this.paging.pageIndex = this.paging.pageIndex + 1;
+    // this.getForeignCourseOtherDocuments();
   }
 
-  applyFilter(searchText: any){ 
+  applyFilter(searchText: any) {
     this.searchText = searchText;
     //this.getForeignCourseOtherDocuments();
-  } 
-
+  }
 
   deleteItem(row) {
-    const id = row.foreignCourseOthersDocumentId; 
-    this.confirmService.confirm('Confirm delete message', 'Are You Sure Delete This Item?').subscribe(result => {
-      if (result) {
-        this.ForeignCourseOtherDocumentService.delete(id).subscribe(() => {
-         // this.getForeignCourseOtherDocuments();
-          this.snackBar.open('Information Deleted Successfully ', '', {
-            duration: 3000,
-            verticalPosition: 'bottom',
-            horizontalPosition: 'right',
-            panelClass: 'snackbar-danger'
+    const id = row.foreignCourseOthersDocumentId;
+    this.confirmService
+      .confirm("Confirm delete message", "Are You Sure Delete This Item?")
+      .subscribe((result) => {
+        if (result) {
+          this.ForeignCourseOtherDocumentService.delete(id).subscribe(() => {
+            // this.getForeignCourseOtherDocuments();
+            this.snackBar.open("Information Deleted Successfully ", "", {
+              duration: 3000,
+              verticalPosition: "bottom",
+              horizontalPosition: "right",
+              panelClass: "snackbar-danger",
+            });
           });
-        })
-      }
-    })
-    
+        }
+      });
   }
 }
--- ClientApp/src/app/air-ticket/service/foreigncourseotherdoc.service.ts
+++ ClientApp/src/app/air-ticket/service/foreigncourseotherdoc.service.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
 
 import { HttpClient, HttpParams } from '@angular/common/http';
-import { environment } from 'src/environments/environment';
+import { environment } from '../../../environments/environment';
 import { IForeignCourseOtherDocPagination,ForeignCourseOtherDocPagination } from '../models/ForeignCourseOtherDocPagination';
 import { ForeignCourseOtherDoc } from '../models/ForeignCourseOtherDoc';
 import { SelectedModel } from '../../core/models/selectedModel';
 import { map } from 'rxjs';
-import { PostResponse } from 'src/app/core/models/PostResponse';
+import { PostResponse } from '../../core/models/PostResponse';
 import { TraineeListForForeignCourseOtherDoc } from '../models/traineeListforforeigncourseotherdoc';
 
 @Injectable({
