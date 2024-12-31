d806517c4ac10ea36d5e93223e97357ab1ba7cbd
Joy Mahanta <joymahanta9@infinitytechltd.com>
2024-12-31 13:13:58 +0600

    Modified the searched trainee, instruction detailed handler to filter out course list based on currently logged in user access

--- SchoolManagement.Application/Features/GlobalSearch/Handlers/Queries/GetSearchedInstructorDetailRequestHandler.cs
+++ SchoolManagement.Application/Features/GlobalSearch/Handlers/Queries/GetSearchedInstructorDetailRequestHandler.cs
@@ -11,6 +11,10 @@ using System.Threading.Tasks;
 using SchoolManagement.Application.DTOs.GlobalSearch;
 using Microsoft.EntityFrameworkCore;
 using SchoolManagement.Application.DTOs.CourseDurations;
+using Microsoft.AspNetCore.Http;
+using System.Runtime.CompilerServices;
+using SchoolManagement.Application.Contracts.Identity;
+using SchoolManagement.Application.Constants;
 
 namespace SchoolManagement.Application.Features.GlobalSearch.Handlers.Queries
 {
@@ -20,16 +24,22 @@ namespace SchoolManagement.Application.Features.GlobalSearch.Handlers.Queries
         private readonly ISchoolManagementRepository<Domain.CourseInstructor> _CourseInstructorRepo;
         private readonly ISchoolManagementRepository<Domain.CourseDuration> _CourseDurationRepo;
         private readonly IMapper _mapper;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly IUserService _userService;
 
         public GetSearchedInstructorDetailRequestHandler(ISchoolManagementRepository<Domain.TraineeBioDataGeneralInfo> TraineeInfoRepo,
            ISchoolManagementRepository<Domain.CourseInstructor> CourseInstructorRepo,
            ISchoolManagementRepository<Domain.CourseDuration> CourseDurationRepo,
-           IMapper mapper)
+           IMapper mapper,
+           IHttpContextAccessor httpContextAccessor,
+           IUserService userService)
         {
             _TraineeInfoRepo = TraineeInfoRepo;
             _CourseInstructorRepo = CourseInstructorRepo;
             _CourseDurationRepo = CourseDurationRepo;
             _mapper = mapper;
+            _httpContextAccessor = httpContextAccessor;
+            _userService = userService;
         }
         public async Task<object> Handle(GetSearchedInstructorDetailRequest request, CancellationToken cancellationToken)
         {
@@ -40,14 +50,38 @@ namespace SchoolManagement.Application.Features.GlobalSearch.Handlers.Queries
                 throw new NotFoundException(nameof(instructor), request.instructorId);
             }
 
+            // Get the search level
+            var userId = _httpContextAccessor.HttpContext.User.FindFirst(CustomClaimTypes.Uid)?.Value;
+
+            List<int?> branchId = new List<int?> { null, null, null };
+            if (userId != null)
+            {
+                var user = await _userService.GetUserById(userId);
+                if (user != null)
+                {
+                    branchId[0] = user.SecondLevel;
+                    branchId[1] = user.ThirdLevel;
+                    branchId[2] = user.FourthLevel;
+                }
+            }
+
             var instructorDetail = new SearchedInstructorDetailDto();
 
             var courseDurationIds = await _CourseInstructorRepo.Where(x => x.TraineeId == request.instructorId).Select(x => x.CourseDurationId).Distinct().ToListAsync();
 
-            var courseDurations = await _CourseDurationRepo.Where(x => courseDurationIds.Contains(x.CourseDurationId))
-                .Include(x=>x.CourseName).ToListAsync();
 
-            instructorDetail.CourseDurations = _mapper.Map<List<CourseDurationDto>>(courseDurations);
+            var courseDurations = _CourseDurationRepo.Where(x => true)
+                .Include(x => x.CourseName)
+                .Include(x => x.BaseSchoolName)
+                .AsQueryable();
+            courseDurations = courseDurations.Where(x => courseDurationIds.Contains(x.CourseDurationId));
+            courseDurations = courseDurations.Where(x =>
+                    (branchId[0] == null || x.BaseSchoolName == null || x.BaseSchoolName.SecondLevel == branchId[0]) &&
+                    (branchId[1] == null || x.BaseSchoolName == null || x.BaseSchoolName.ThirdLevel == branchId[1]) &&
+                    (branchId[2] == null || x.BaseSchoolName == null || x.BaseSchoolName.FourthLevel == branchId[2])
+            );
+
+            instructorDetail.CourseDurations = _mapper.Map<List<CourseDurationDto>>(await courseDurations.ToListAsync());
             instructorDetail.TotalCourse = instructorDetail.CourseDurations.Count();
             instructorDetail.PreviousCourse = instructorDetail.CourseDurations.Where(x => x.DurationTo < DateTime.Now).Count();
             instructorDetail.RunningCourse = instructorDetail.CourseDurations.Where(x => x.DurationFrom <= DateTime.Now && x.DurationTo >= DateTime.Now).Count();
--- SchoolManagement.Application/Features/GlobalSearch/Handlers/Queries/GetSearchedTraineeDetailRequestHandler.cs
+++ SchoolManagement.Application/Features/GlobalSearch/Handlers/Queries/GetSearchedTraineeDetailRequestHandler.cs
@@ -11,6 +11,9 @@ using SchoolManagement.Application.DTOs.GlobalSearch;
 using AutoMapper;
 using Microsoft.EntityFrameworkCore;
 using SchoolManagement.Application.DTOs.CourseDurations;
+using Microsoft.AspNetCore.Http;
+using SchoolManagement.Application.Constants;
+using SchoolManagement.Application.Contracts.Identity;
 
 namespace SchoolManagement.Application.Features.GlobalSearch.Handlers.Queries
 {
@@ -20,15 +23,21 @@ namespace SchoolManagement.Application.Features.GlobalSearch.Handlers.Queries
         private readonly ISchoolManagementRepository<Domain.TraineeBioDataGeneralInfo> _TraineeInfoRepo;
         private readonly ISchoolManagementRepository<Domain.TraineeNomination> _TraineeNominationRepo;
         private readonly IMapper _mapper;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly IUserService _userService;
         public GetSearchedTraineeDetailRequestHandler(ISchoolManagementRepository<Domain.CourseDuration> CourseDurationRepo,
             ISchoolManagementRepository<Domain.TraineeBioDataGeneralInfo> TraineeInfoRepo,
             ISchoolManagementRepository<Domain.TraineeNomination> TraineeNominationRepo,
-            IMapper mapper)
+            IMapper mapper,
+            IHttpContextAccessor httpContextAccessor,
+            IUserService userService)
         {
             _CourseDurationRepo = CourseDurationRepo;
             _TraineeInfoRepo = TraineeInfoRepo;
             _TraineeNominationRepo = TraineeNominationRepo;
             _mapper = mapper;
+            _httpContextAccessor = httpContextAccessor;
+            _userService = userService;
         }
 
         public async Task<object> Handle(GetSearchedTraineeDetailRequest request, CancellationToken cancellationToken)
@@ -41,14 +50,36 @@ namespace SchoolManagement.Application.Features.GlobalSearch.Handlers.Queries
                 throw new NotFoundException(nameof(trainee), request.TraineeId);
             }
 
+            // Get the search level
+            var userId = _httpContextAccessor.HttpContext.User.FindFirst(CustomClaimTypes.Uid)?.Value;
+
+            List<int?> branchId = new List<int?> { null,null,null};
+            if (userId != null)
+            {
+                var user = await _userService.GetUserById(userId);
+                if(user!=null)
+                {
+                    branchId[0] = user.SecondLevel;
+                    branchId[1] = user.ThirdLevel;
+                    branchId[2] = user.FourthLevel;
+                }
+            }
+
             var traineeDetail = new SearchedTraineeDetailDto();
 
             List<int?> courseDurationIds = await _TraineeNominationRepo.Where(x => x.TraineeId == request.TraineeId && x.TraineeId != null).Select(x =>x.CourseDurationId).Distinct().ToListAsync();
 
-            var courseDurations = await _CourseDurationRepo.Where(x => courseDurationIds.Contains(x.CourseDurationId))
-                .Include(x=>x.CourseName).ToListAsync();
+            var courseDurations = _CourseDurationRepo.Where(x => true)
+                    .Include(x => x.CourseName)
+                    .Include(x => x.BaseSchoolName).AsQueryable();
+            courseDurations = courseDurations.Where(x => courseDurationIds.Contains(x.CourseDurationId));
+            courseDurations = courseDurations.Where(x =>
+                    (branchId[0] == null || x.BaseSchoolName == null || x.BaseSchoolName.SecondLevel == branchId[0]) &&
+                    (branchId[1] == null || x.BaseSchoolName == null || x.BaseSchoolName.ThirdLevel == branchId[1]) &&
+                    (branchId[2] == null || x.BaseSchoolName == null || x.BaseSchoolName.FourthLevel == branchId[2])
+            );
 
-            traineeDetail.CourseDurations = _mapper.Map<List<CourseDurationDto>>(courseDurations);
+            traineeDetail.CourseDurations = _mapper.Map<List<CourseDurationDto>>(await courseDurations.ToListAsync());
             traineeDetail.TotalCourse = traineeDetail.CourseDurations.Count();
             traineeDetail.PreviousCourse = traineeDetail.CourseDurations.Where(x => x.DurationTo < DateTime.Now).Count();
             traineeDetail.RunningCourse = traineeDetail.CourseDurations.Where(x=> x.DurationFrom <= DateTime.Now && x.DurationTo >= DateTime.Now).Count();
