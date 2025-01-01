8e0a80c65e6e8384df6fb84fd9c02d4abfd04062
shahadat <shahadathossan937@gmail.com>
2025-01-01 17:10:22 +0600

    refactor: enhance code formatting and readability in NewComplexionComponent

--- ClientApp/src/app/basic-setup/complexion/new-complexion/new-complexion.component.ts
+++ ClientApp/src/app/basic-setup/complexion/new-complexion/new-complexion.component.ts
@@ -1,22 +1,25 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { ActivatedRoute, Router } from '@angular/router';
-import { ComplexionService } from '../../service/complexion.service';
-import { MatSnackBar } from '@angular/material/snack-bar';
-import { ConfirmService } from '../../../core/service/confirm.service';
-import { UnsubscribeOnDestroyAdapter } from 'src/app/shared/UnsubscribeOnDestroyAdapter';
-import { SharedServiceService } from 'src/app/shared/shared-service.service';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { ActivatedRoute, Router } from "@angular/router";
+import { ComplexionService } from "../../service/complexion.service";
+import { MatSnackBar } from "@angular/material/snack-bar";
+import { ConfirmService } from "../../../core/service/confirm.service";
+import { SharedServiceService } from "../../../shared/shared-service.service";
+import { UnsubscribeOnDestroyAdapter } from "../../../shared/UnsubscribeOnDestroyAdapter";
 
-@Component({ 
-  selector: 'app-new-complexion',
-  templateUrl: './new-complexion.component.html',
-  styleUrls: ['./new-complexion.component.sass']
+@Component({
+  selector: "app-new-complexion",
+  templateUrl: "./new-complexion.component.html",
+  styleUrls: ["./new-complexion.component.sass"],
 })
-export class NewComplexionComponent extends UnsubscribeOnDestroyAdapter implements OnInit {
-  buttonText:string;
+export class NewComplexionComponent
+  extends UnsubscribeOnDestroyAdapter
+  implements OnInit
+{
+  buttonText: string;
   loading = false;
   pageTitle: string;
-  destination:string;
+  destination: string;
   complexionForm: FormGroup;
   validationErrors: string[] = [];
 
@@ -24,81 +27,89 @@ export class NewComplexionComponent extends UnsubscribeOnDestroyAdapter implemen
     private snackBar: MatSnackBar,
     private confirmService: ConfirmService,
     private complexionService: ComplexionService,
-    private fb: FormBuilder, 
-    private router: Router,  
+    private fb: FormBuilder,
+    private router: Router,
     private route: ActivatedRoute,
-    public sharedService: SharedServiceService) {
+    public sharedService: SharedServiceService
+  ) {
     super();
   }
 
   ngOnInit(): void {
-    const id = this.route.snapshot.paramMap.get('complexionId'); 
+    const id = this.route.snapshot.paramMap.get("complexionId");
     if (id) {
-      this.pageTitle = 'Edit Complexion';
+      this.pageTitle = "Edit Complexion";
       this.destination = "Edit";
-      this.buttonText= "Update"
-      this.complexionService.find(+id).subscribe(
-        res => {
-          this.complexionForm.patchValue({          
-
-            complexionId: res.complexionId,
-            complexionName: res.complexionName,
-         //   menuPosition: res.menuPosition,
-          });          
-        }
-      );
+      this.buttonText = "Update";
+      this.complexionService.find(+id).subscribe((res) => {
+        this.complexionForm.patchValue({
+          complexionId: res.complexionId,
+          complexionName: res.complexionName,
+          //   menuPosition: res.menuPosition,
+        });
+      });
     } else {
-      this.pageTitle = 'Create Complexion';
+      this.pageTitle = "Create Complexion";
       this.destination = "Add";
-      this.buttonText= "Save"
+      this.buttonText = "Save";
     }
     this.intitializeForm();
   }
   intitializeForm() {
     this.complexionForm = this.fb.group({
       complexionId: [0],
-      complexionName: ['', Validators.required],
-    //  menuPosition: ['', Validators.required],
+      complexionName: ["", Validators.required],
+      //  menuPosition: ['', Validators.required],
       isActive: [true],
-    
-    })
+    });
   }
-  
-  onSubmit() {
-    const id = this.complexionForm.get('complexionId').value;   
-    if (id) {
-      this.confirmService.confirm('Confirm Update message', 'Are You Sure Update This  Item').subscribe(result => {
-        if (result) {
-          this.loading=true;
-          this.complexionService.update(+id,this.complexionForm.value).subscribe(response => {
-            this.router.navigateByUrl('/basic-setup/complexion-list');
-            this.snackBar.open('Complexion Information Updated Successfully ', '', {
-              duration: 2000,
-              verticalPosition: 'bottom',
-              horizontalPosition: 'right',
-              panelClass: 'snackbar-success'
-            });
-          }, error => {
-            this.validationErrors = error;
-          })
-        }
-      })
-    }
 
-     else {
-      this.loading=true;
-      this.complexionService.submit(this.complexionForm.value).subscribe(response => {
-        this.router.navigateByUrl('/basic-setup/complexion-list');
-        this.snackBar.open('Complexion Information Saved Successfully ', '', {
-          duration: 2000,
-          verticalPosition: 'bottom',
-          horizontalPosition: 'right',
-          panelClass: 'snackbar-success'
+  onSubmit() {
+    const id = this.complexionForm.get("complexionId")?.value;
+    if (id) {
+      this.confirmService
+        .confirm("Confirm Update message", "Are You Sure Update This  Item")
+        .subscribe((result) => {
+          if (result) {
+            this.loading = true;
+            this.complexionService
+              .update(+id, this.complexionForm.value)
+              .subscribe(
+                (response) => {
+                  this.router.navigateByUrl("/basic-setup/complexion-list");
+                  this.snackBar.open(
+                    "Complexion Information Updated Successfully ",
+                    "",
+                    {
+                      duration: 2000,
+                      verticalPosition: "bottom",
+                      horizontalPosition: "right",
+                      panelClass: "snackbar-success",
+                    }
+                  );
+                },
+                (error) => {
+                  this.validationErrors = error;
+                }
+              );
+          }
         });
-      }, error => {
-        this.validationErrors = error;
-      })
+    } else {
+      this.loading = true;
+      this.complexionService.submit(this.complexionForm.value).subscribe(
+        (response) => {
+          this.router.navigateByUrl("/basic-setup/complexion-list");
+          this.snackBar.open("Complexion Information Saved Successfully ", "", {
+            duration: 2000,
+            verticalPosition: "bottom",
+            horizontalPosition: "right",
+            panelClass: "snackbar-success",
+          });
+        },
+        (error) => {
+          this.validationErrors = error;
+        }
+      );
     }
- 
   }
 }
