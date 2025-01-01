683afbc9806bbf37e317432e1878cf8f9af40039
shahadat <shahadathossan937@gmail.com>
2025-01-01 17:27:53 +0600

    refactor: enhance code formatting and readability in NewCourseModuleComponent and CourseModuleListComponent

--- ClientApp/src/app/basic-setup/coursemodule/coursemodule-list/coursemodule-list.component.ts
+++ ClientApp/src/app/basic-setup/coursemodule/coursemodule-list/coursemodule-list.component.ts
@@ -1,25 +1,26 @@
-import { SelectionModel } from '@angular/cdk/collections';
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-import { MatPaginator, PageEvent } from '@angular/material/paginator';
-import { MatTableDataSource } from '@angular/material/table';
-import { CourseModule } from '../../models/CourseModule';
-import { CourseModuleService } from '../../service/CourseModule.service';
-import { ConfirmService } from 'src/app/core/service/confirm.service';
-import { ActivatedRoute, Router } from '@angular/router';
-import { MasterData } from 'src/assets/data/master-data';
-import { MatSnackBar } from '@angular/material/snack-bar';
-import { UnsubscribeOnDestroyAdapter } from 'src/app/shared/UnsubscribeOnDestroyAdapter';
-import { SharedServiceService } from 'src/app/shared/shared-service.service';
-
+import { SelectionModel } from "@angular/cdk/collections";
+import { Component, ElementRef, OnInit, ViewChild } from "@angular/core";
+import { MatPaginator, PageEvent } from "@angular/material/paginator";
+import { MatTableDataSource } from "@angular/material/table";
+import { CourseModule } from "../../models/CourseModule";
+import { CourseModuleService } from "../../service/CourseModule.service";
+import { ActivatedRoute, Router } from "@angular/router";
+import { MatSnackBar } from "@angular/material/snack-bar";
+import { MasterData } from "../../../../assets/data/master-data";
+import { ConfirmService } from "../../../core/service/confirm.service";
+import { SharedServiceService } from "../../../shared/shared-service.service";
+import { UnsubscribeOnDestroyAdapter } from "../../../shared/UnsubscribeOnDestroyAdapter";
 
 @Component({
-  selector: 'app-coursemodule-list',
-  templateUrl: './coursemodule-list.component.html',
-  styleUrls: ['./coursemodule-list.component.sass']
+  selector: "app-coursemodule-list",
+  templateUrl: "./coursemodule-list.component.html",
+  styleUrls: ["./coursemodule-list.component.sass"],
 })
-export class CourseModuleListComponent extends UnsubscribeOnDestroyAdapter implements OnInit {
-
-   masterData = MasterData;
+export class CourseModuleListComponent
+  extends UnsubscribeOnDestroyAdapter
+  implements OnInit
+{
+  masterData = MasterData;
   loading = false;
   ELEMENT_DATA: CourseModule[] = [];
   isLoading = false;
@@ -27,39 +28,48 @@ export class CourseModuleListComponent extends UnsubscribeOnDestroyAdapter imple
   paging = {
     pageIndex: this.masterData.paging.pageIndex,
     pageSize: this.masterData.paging.pageSize,
-    length: 1
-  }
-  searchText="";
+    length: 1,
+  };
+  searchText = "";
 
-  displayedColumns: string[] = [ 'sl',/*'courseModuleId',*/ 'baseSchoolName', 'courseName', 'moduleName','nameOfModule', /*'menuPosition',*/ 'actions'];
+  displayedColumns: string[] = [
+    "sl",
+    /*'courseModuleId',*/ "baseSchoolName",
+    "courseName",
+    "moduleName",
+    "nameOfModule",
+    /*'menuPosition',*/ "actions",
+  ];
   dataSource: MatTableDataSource<CourseModule> = new MatTableDataSource();
 
   selection = new SelectionModel<CourseModule>(true, []);
 
-  
   constructor(
     private route: ActivatedRoute,
     private snackBar: MatSnackBar,
     private CourseModuleService: CourseModuleService,
     private router: Router,
     private confirmService: ConfirmService,
-    public sharedService: SharedServiceService) {
+    public sharedService: SharedServiceService
+  ) {
     super();
   }
-  
+
   ngOnInit() {
     this.getCourseModules();
   }
-  
+
   getCourseModules() {
     this.isLoading = true;
-    this.CourseModuleService.getCourseModules(this.paging.pageIndex, this.paging.pageSize,this.searchText).subscribe(response => {
-     
-
-      this.dataSource.data = response.items; 
-      this.paging.length = response.totalItemsCount    
+    this.CourseModuleService.getCourseModules(
+      this.paging.pageIndex,
+      this.paging.pageSize,
+      this.searchText
+    ).subscribe((response) => {
+      this.dataSource.data = response.items;
+      this.paging.length = response.totalItemsCount;
       this.isLoading = false;
-    })
+    });
   }
   isAllSelected() {
     const numSelected = this.selection.selected.length;
@@ -74,38 +84,39 @@ export class CourseModuleListComponent extends UnsubscribeOnDestroyAdapter imple
           this.selection.select(row)
         );
   }
-  addNew(){
-    
-  }
- 
+  addNew() {}
+
   pageChanged(event: PageEvent) {
-    this.paging.pageIndex = event.pageIndex
-    this.paging.pageSize = event.pageSize
-    this.paging.pageIndex = this.paging.pageIndex + 1
+    this.paging.pageIndex = event.pageIndex;
+    this.paging.pageSize = event.pageSize;
+    this.paging.pageIndex = this.paging.pageIndex + 1;
     this.getCourseModules();
   }
 
-  applyFilter(searchText: any){ 
+  applyFilter(searchText: any) {
     this.searchText = searchText;
     this.getCourseModules();
-  } 
-
+  }
 
   deleteItem(row) {
-    const id = row.courseModuleId; 
-    this.confirmService.confirm('Confirm delete message', 'Are You Sure Delete This Course Module Item?').subscribe(result => {
-      if (result) {
-        this.CourseModuleService.delete(id).subscribe(() => {
-          this.getCourseModules();
-          this.snackBar.open('Information Deleted Successfully ', '', {
-            duration: 3000,
-            verticalPosition: 'bottom',
-            horizontalPosition: 'right',
-            panelClass: 'snackbar-danger'
+    const id = row.courseModuleId;
+    this.confirmService
+      .confirm(
+        "Confirm delete message",
+        "Are You Sure Delete This Course Module Item?"
+      )
+      .subscribe((result) => {
+        if (result) {
+          this.CourseModuleService.delete(id).subscribe(() => {
+            this.getCourseModules();
+            this.snackBar.open("Information Deleted Successfully ", "", {
+              duration: 3000,
+              verticalPosition: "bottom",
+              horizontalPosition: "right",
+              panelClass: "snackbar-danger",
+            });
           });
-        })
-      }
-    })
-    
+        }
+      });
   }
 }
