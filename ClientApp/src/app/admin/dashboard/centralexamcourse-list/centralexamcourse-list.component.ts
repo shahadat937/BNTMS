91e35bdfeaa0a5a850239732ba36dc984b23c83d
arafatshuvo1-beep <arafatshuvo18@infinitytechltd.com>
2025-01-07 16:12:50 +0600

    remove error from t

--- ClientApp/src/app/admin/dashboard/centralexamcourse-list/centralexamcourse-list.component.ts
+++ ClientApp/src/app/admin/dashboard/centralexamcourse-list/centralexamcourse-list.component.ts
@@ -5,12 +5,12 @@ import {BNAExamInstructorAssign} from '../../../exam-management/models/bnaexamin
 import {BNAExamInstructorAssignService} from '../../../exam-management/service/bnaexaminstructorassign.service';
 import { SelectionModel, DataSource } from '@angular/cdk/collections';
 import { ActivatedRoute, Router } from '@angular/router';
-import { ConfirmService } from 'src/app/core/service/confirm.service';
-import {MasterData} from 'src/assets/data/master-data';
+import { ConfirmService } from '../../../../../src/app/core/service/confirm.service';
+import {MasterData} from '../../../../../src/assets/data/master-data';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { dashboardService } from '../services/dashboard.service'
-import { UnsubscribeOnDestroyAdapter } from 'src/app/shared/UnsubscribeOnDestroyAdapter';
-import { SharedServiceService } from 'src/app/shared/shared-service.service';
+import { UnsubscribeOnDestroyAdapter } from '../../../../../src/app/shared/UnsubscribeOnDestroyAdapter';
+import { SharedServiceService } from '../../../../../src/app/shared/shared-service.service';
 
 @Component({
   selector: 'app-centralexamcourse-list',
