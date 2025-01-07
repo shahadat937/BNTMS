d5b5096556f649395e97ce464df2d9c75c5bbcd1
arafatshuvo1-beep <arafatshuvo18@infinitytechltd.com>
2025-01-07 16:34:11 +0600

    remove error from exam instructor list

--- ClientApp/src/app/admin/dashboard/examinstructor-list/examinstructor-list.component.ts
+++ ClientApp/src/app/admin/dashboard/examinstructor-list/examinstructor-list.component.ts
@@ -5,11 +5,11 @@ import {BNAExamInstructorAssign} from '../../../exam-management/models/bnaexamin
 import {BNAExamInstructorAssignService} from '../../../exam-management/service/bnaexaminstructorassign.service';
 import { SelectionModel } from '@angular/cdk/collections';
 import { ActivatedRoute, Router } from '@angular/router';
-import { ConfirmService } from 'src/app/core/service/confirm.service';
-import {MasterData} from 'src/assets/data/master-data';
+import { ConfirmService } from '../../../../../src/app/core/service/confirm.service';
+import {MasterData} from '../../../../../src/assets/data/master-data';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { UnsubscribeOnDestroyAdapter } from 'src/app/shared/UnsubscribeOnDestroyAdapter';
-import { SharedServiceService } from 'src/app/shared/shared-service.service';
+import { UnsubscribeOnDestroyAdapter } from '../../../../../src/app/shared/UnsubscribeOnDestroyAdapter';
+import { SharedServiceService } from '../../../../../src/app/shared/shared-service.service';
 
 @Component({
   selector: 'app-examinstructor-list',
