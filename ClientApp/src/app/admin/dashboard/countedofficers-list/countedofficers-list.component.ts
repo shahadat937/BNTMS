f3df1d4860c3c84d81e5f2c29d7329ab4ee720f6
arafatshuvo1-beep <arafatshuvo18@infinitytechltd.com>
2025-01-07 16:20:53 +0600

    remove error from course terminated

--- ClientApp/src/app/admin/dashboard/countedofficers-list/countedofficers-list.component.ts
+++ ClientApp/src/app/admin/dashboard/countedofficers-list/countedofficers-list.component.ts
@@ -6,13 +6,13 @@ import {TraineeNominationService} from '../../../course-management/service/train
 import { SpOfficerDetails } from '../models/spofficerdetails';
 import { SelectionModel } from '@angular/cdk/collections';
 import { ActivatedRoute, Router } from '@angular/router';
-import { ConfirmService } from 'src/app/core/service/confirm.service';
-import {MasterData} from 'src/assets/data/master-data'
+import { ConfirmService } from '../../../../../src/app/core/service/confirm.service';
+import {MasterData} from '../../../../../src/assets/data/master-data'
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { DatePipe } from '@angular/common';
 import {dashboardService} from '../services/dashboard.service';
-import { UnsubscribeOnDestroyAdapter } from 'src/app/shared/UnsubscribeOnDestroyAdapter';
-import { SharedServiceService } from 'src/app/shared/shared-service.service';
+import { UnsubscribeOnDestroyAdapter } from '../../../../../src/app/shared/UnsubscribeOnDestroyAdapter';
+import { SharedServiceService } from '../../../../../src/app/shared/shared-service.service';
 
 @Component({
   selector: 'app-countedofficers-list',
