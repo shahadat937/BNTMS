1454af6623e1a9f50ba88ab845548c7677c8ad62
arafatshuvo1-beep <arafatshuvo18@infinitytechltd.com>
2025-01-07 16:33:18 +0600

    remove error from course week by duration

--- ClientApp/src/app/admin/dashboard/courseweekbyduration-list/courseweekbyduration-list.component.ts
+++ ClientApp/src/app/admin/dashboard/courseweekbyduration-list/courseweekbyduration-list.component.ts
@@ -5,14 +5,14 @@ import {CourseInstructor} from '../../../subject-management/models/courseinstruc
 import {CourseInstructorService} from '../../../subject-management/service/courseinstructor.service';
 import { SelectionModel } from '@angular/cdk/collections';
 import { ActivatedRoute, Router } from '@angular/router';
-import { ConfirmService } from 'src/app/core/service/confirm.service';
-import {MasterData} from 'src/assets/data/master-data';
+import { ConfirmService } from '../../../../../src/app/core/service/confirm.service';
+import {MasterData} from '../../../../../src/assets/data/master-data';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { AuthService } from 'src/app/core/service/auth.service';
-import { Role } from 'src/app/core/models/role';
+import { AuthService } from '../../../../../src/app/core/service/auth.service';
+import { Role } from '../../../../../src/app/core/models/role';
 import { DatePipe } from '@angular/common';
-import { UnsubscribeOnDestroyAdapter } from 'src/app/shared/UnsubscribeOnDestroyAdapter';
-import { SharedServiceService } from 'src/app/shared/shared-service.service';
+import { UnsubscribeOnDestroyAdapter } from '../../../../../src/app/shared/UnsubscribeOnDestroyAdapter';
+import { SharedServiceService } from '../../../../../src/app/shared/shared-service.service';
 
 @Component({
   selector: 'app-courseweekbyduration-list',
