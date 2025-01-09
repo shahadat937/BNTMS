b362e608dfcca61378e1cf50cf7fa5b0662e45d4
arafatshuvo1-beep <arafatshuvo18@infinitytechltd.com>
2025-01-09 11:55:30 +0600

    remove error from bna attendance period

--- ClientApp/src/app/basic-setup/bnaattendanceperiod/new-bnaattendanceperiod/new-bnaattendanceperiod.component.ts
+++ ClientApp/src/app/basic-setup/bnaattendanceperiod/new-bnaattendanceperiod/new-bnaattendanceperiod.component.ts
@@ -2,10 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { ActivatedRoute, Router } from '@angular/router';
-import { ConfirmService } from 'src/app/core/service/confirm.service';
+import { ConfirmService } from '../../../../../src/app/core/service/confirm.service';
 import { BnaAttendancePeriodService } from '../../service/BnaAttendancePeriod.service';
-import { UnsubscribeOnDestroyAdapter } from 'src/app/shared/UnsubscribeOnDestroyAdapter'
-import { SharedServiceService } from 'src/app/shared/shared-service.service';
+import { UnsubscribeOnDestroyAdapter } from '../../../../../src/app/shared/UnsubscribeOnDestroyAdapter'
+import { SharedServiceService } from '../../../../../src/app/shared/shared-service.service';
 
 @Component({
   selector: 'app-new-BnaAttendancePeriod',
@@ -66,7 +66,7 @@ export class NewBnaAttendancePeriodComponent extends UnsubscribeOnDestroyAdapter
   }
   
   onSubmit() {
-    const id = this.BnaAttendancePeriodForm.get('bnaAttendancePeriodId').value;
+    const id = this.BnaAttendancePeriodForm.get('bnaAttendancePeriodId')?.value;
     if (id) {
       this.confirmService.confirm('Confirm Update message', 'Are You Sure Update This Item?').subscribe(result => {
         if (result) {
