3af337e820896cbfd153bf95018d1b98ce0cd537
arafatshuvo1-beep <arafatshuvo18@infinitytechltd.com>
2025-01-09 13:39:56 +0600

    remove error from ocupation

--- ClientApp/src/app/basic-setup/occupation/new-occupation/new-occupation.component.ts
+++ ClientApp/src/app/basic-setup/occupation/new-occupation/new-occupation.component.ts
@@ -2,10 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { ActivatedRoute, Router } from '@angular/router';
-import { ConfirmService } from 'src/app/core/service/confirm.service';
+import { ConfirmService } from '../../../../../src/app/core/service/confirm.service';
 import { OccupationService } from '../../service/occupation.service';
-import { UnsubscribeOnDestroyAdapter } from 'src/app/shared/UnsubscribeOnDestroyAdapter';
-import { SharedServiceService } from 'src/app/shared/shared-service.service';
+import { UnsubscribeOnDestroyAdapter } from '../../../../../src/app/shared/UnsubscribeOnDestroyAdapter';
+import { SharedServiceService } from '../../../../../src/app/shared/shared-service.service';
 
 @Component({
   selector: 'app-new-occupation',
@@ -66,7 +66,7 @@ export class NewOccupationComponent extends UnsubscribeOnDestroyAdapter implemen
   }
   
   onSubmit() {
-    const id = this.occupationForm.get('occupationId').value;
+    const id = this.occupationForm.get('occupationId')?.value;
     if (id) {
       this.confirmService.confirm('Confirm Update message', 'Are You Sure Update This Occupation Item').subscribe(result => {
         if (result) {
