ab7d9b4186c8925690884ee247637ee82f98cb70
arafatshuvo1-beep <arafatshuvo18@infinitytechltd.com>
2025-01-11 11:57:44 +0600

    remove error from course duration

--- ClientApp/src/app/course-management/courseduration/new-courseduration/new-courseduration.component.ts
+++ ClientApp/src/app/course-management/courseduration/new-courseduration/new-courseduration.component.ts
@@ -2,13 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { CourseDurationService } from '../../service/courseduration.service';
-import { SelectedModel } from 'src/app/core/models/selectedModel';
-import { CodeValueService } from 'src/app/basic-setup/service/codevalue.service';
-import { MasterData } from 'src/assets/data/master-data';
+import { SelectedModel } from '../../../../../src/app/core/models/selectedModel';
+import { CodeValueService } from '../../../../../src/app/basic-setup/service/codevalue.service';
+import { MasterData } from '../../../../../src/assets/data/master-data';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { ConfirmService } from 'src/app/core/service/confirm.service';
-import { UnsubscribeOnDestroyAdapter } from 'src/app/shared/UnsubscribeOnDestroyAdapter';
-import { SharedServiceService } from 'src/app/shared/shared-service.service';
+import { ConfirmService } from '../../../../../src/app/core/service/confirm.service';
+import { UnsubscribeOnDestroyAdapter } from '../../../../../src/app/shared/UnsubscribeOnDestroyAdapter';
+import { SharedServiceService } from '../../../../../src/app/shared/shared-service.service';
 
 @Component({
   selector: 'app-new-courseduration',
@@ -122,7 +122,7 @@ export class NewCourseDurationComponent extends UnsubscribeOnDestroyAdapter impl
   }
 
   onSubmit() {
-    const id = this.CourseDurationForm.get('courseDurationId').value;   
+    const id = this.CourseDurationForm.get('courseDurationId')?.value;   
     if (id) {
       this.confirmService.confirm('Confirm Update message', 'Are You Sure Update This  Item').subscribe(result => {
         if (result) {
