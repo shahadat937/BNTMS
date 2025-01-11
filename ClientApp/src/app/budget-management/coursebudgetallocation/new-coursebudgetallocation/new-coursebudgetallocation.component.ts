309244a9a499aa14f59c076d8269d0e7eb078a5f
arafatshuvo1-beep <arafatshuvo18@infinitytechltd.com>
2025-01-11 11:15:04 +0600

    remove error from budget management

--- ClientApp/src/app/budget-management/coursebudgetallocation/new-coursebudgetallocation/new-coursebudgetallocation.component.ts
+++ ClientApp/src/app/budget-management/coursebudgetallocation/new-coursebudgetallocation/new-coursebudgetallocation.component.ts
@@ -1,21 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Role } from 'src/app/core/models/role';
+import { Role } from '../../../../../src/app/core/models/role';
 import { ActivatedRoute, Router } from '@angular/router';
 import { CourseBudgetAllocationService } from '../../service/courseBudgetAllocation.service';
-import { SelectedModel } from 'src/app/core/models/selectedModel';
-import { CodeValueService } from 'src/app/basic-setup/service/codevalue.service';
-import { MasterData } from 'src/assets/data/master-data';
+import { SelectedModel } from '../../../../../src/app/core/models/selectedModel';
+import { CodeValueService } from '../../../../../src/app/basic-setup/service/codevalue.service';
+import { MasterData } from '../../../../../src/assets/data/master-data';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { ConfirmService } from 'src/app/core/service/confirm.service';
-import { CourseWeekService } from 'src/app/course-management/service/CourseWeek.service';
+import { ConfirmService } from '../../../../../src/app/core/service/confirm.service';
+import { CourseWeekService } from '../../../../../src/app/course-management/service/CourseWeek.service';
 import { CourseBudgetAllocation } from '../../models/courseBudgetAllocation';
 import { MatPaginator, PageEvent } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
 import { SelectionModel } from '@angular/cdk/collections';
-import { AuthService } from 'src/app/core/service/auth.service';
-import { UnsubscribeOnDestroyAdapter } from 'src/app/shared/UnsubscribeOnDestroyAdapter';
-import { SharedServiceService } from 'src/app/shared/shared-service.service';
+import { AuthService } from '../../../../../src/app/core/service/auth.service';
+import { UnsubscribeOnDestroyAdapter } from '../../../../../src/app/shared/UnsubscribeOnDestroyAdapter';
+import { SharedServiceService } from '../../../../../src/app/shared/shared-service.service';
 
 @Component({
   selector: 'app-new-coursebudgetallocation',
@@ -163,7 +163,7 @@ export class NewCourseBudgetAllocationComponent extends UnsubscribeOnDestroyAdap
   onTraineeSelectionChange(dropdown){
     if (dropdown.isUserInput) {
       this.traineeId=dropdown.source.value;
-      this.courseNameId =  this.CourseBudgetAllocationForm.get('courseNameId').value;
+      this.courseNameId =  this.CourseBudgetAllocationForm.get('courseNameId')?.value;
       this.getCourseBudgetAllocationList();
     }
   }
@@ -172,7 +172,7 @@ export class NewCourseBudgetAllocationComponent extends UnsubscribeOnDestroyAdap
     if (dropdown.isUserInput) {
        var budgetCodeId = dropdown.source.value.value; 
 
-       this.CourseBudgetAllocationForm.get('budgetCodeId').setValue(budgetCodeId);
+       this.CourseBudgetAllocationForm.get('budgetCodeId')?.setValue(budgetCodeId);
 
        this.CourseBudgetAllocationService.getTotalBudgetByBudgetCodeIdRequest(budgetCodeId).subscribe(res=>{
        this.totalBudget=res[0].text; 
@@ -200,8 +200,8 @@ export class NewCourseBudgetAllocationComponent extends UnsubscribeOnDestroyAdap
    });
    
 
-    this.CourseBudgetAllocationForm.get('courseNameId').setValue(courseNameId);
-    this.CourseBudgetAllocationForm.get('courseDurationId').setValue(courseDurationId);
+    this.CourseBudgetAllocationForm.get('courseNameId')?.setValue(courseNameId);
+    this.CourseBudgetAllocationForm.get('courseDurationId')?.setValue(courseDurationId);
 }
 
   getCourseBudgetAllocationList(){
@@ -305,7 +305,7 @@ export class NewCourseBudgetAllocationComponent extends UnsubscribeOnDestroyAdap
   }
 
   onSubmit() {
-    const id = this.CourseBudgetAllocationForm.get('courseBudgetAllocationId').value; 
+    const id = this.CourseBudgetAllocationForm.get('courseBudgetAllocationId')?.value; 
     if (id) {
       this.confirmService.confirm('Confirm Update message', 'Are You Sure Update This  Item?').subscribe(result => {
         if (result) {
