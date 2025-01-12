517ab9f96b5ea190e5568ab25fe2b2ee868f938e
infinityaziz <abdulaziz@infinitytechltd.com>
2025-01-12 17:38:28 +0600

    working on execl upload in service instructor

---
+++ SchoolManagement.Application/Features/TraineeBIODataGeneralInfos/Handlers/Commands/UploadServiceInstructorCommandHandler.cs
@@ -0,0 +1,123 @@
+﻿using MediatR;
+using OfficeOpenXml;
+using SchoolManagement.Application.Contracts.Identity;
+using SchoolManagement.Application.Contracts.Persistence;
+using SchoolManagement.Application.DTOs.User;
+using SchoolManagement.Application.Features.TraineeBIODataGeneralInfos.Requests.Commands;
+using SchoolManagement.Application.Responses;
+using SchoolManagement.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolManagement.Application.Features.TraineeBIODataGeneralInfos.Handlers.Commands
+{
+    public class UploadServiceInstructorCommandHandler : IRequestHandler<UploadServiceInstructorCommand, BaseCommandResponse>
+    {
+        private readonly ISchoolManagementRepository<AspNetUsers> _aspUserRepository;
+        private readonly ISchoolManagementRepository<TraineeBioDataGeneralInfo> _traineeBioDataGeneralInfo;
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IUserService _userService;
+
+        public UploadServiceInstructorCommandHandler(ISchoolManagementRepository<AspNetUsers> aspUserRepository, IUnitOfWork unitOfWork, ISchoolManagementRepository<TraineeBioDataGeneralInfo> traineeBioDataGeneralInfo, IUserService userService)
+        {
+            _aspUserRepository = aspUserRepository;
+            _unitOfWork = unitOfWork;
+            _traineeBioDataGeneralInfo = traineeBioDataGeneralInfo;
+            _userService = userService;
+        }
+        public async Task<BaseCommandResponse> Handle(UploadServiceInstructorCommand request, CancellationToken cancellationToken)
+        {
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+            var response = new BaseCommandResponse();
+            var successCount = 0;
+            var errorCount = 0;
+
+
+            using (var stream = new MemoryStream())
+            {
+                await request.ServiceInstructorFile.CopyToAsync(stream);
+                using (var package = new ExcelPackage(stream))
+                {
+                    var worksheet = package.Workbook.Worksheets[0];
+                    var rowCount = worksheet.Dimension.Rows;
+                    for (int row = 2; row <= rowCount; row++)
+                    {
+                        var cellValue = worksheet.Cells[row, 2].Text;
+
+                        if (string.IsNullOrWhiteSpace(cellValue))
+                        {
+                            break;
+                        }
+                        var pno = worksheet.Cells[row, 2].Text;
+
+                        TraineeBioDataGeneralInfo traineeBioData = await _traineeBioDataGeneralInfo.FindOneAsync(x => x.Pno == pno);
+
+                        if (traineeBioData != null)
+                        {
+                            var instructor = _userService.GetUserByTraineeId(traineeBioData.TraineeId.ToString());
+
+                           
+                           
+                        }
+                        else
+                        {
+
+                            errorCount++;
+
+                        }
+
+
+
+
+                        //    if (instructor != null)
+                        //    {
+                        //        if (instructor.BranchId != null)
+                        //        {
+                        //            response.Success = false;
+                        //            response.Message = "Creation Failed, Trainee already exist";
+                        //            errorCount++;
+                        //        }
+                        //        else
+                        //        {
+                        //            await _unitOfWork.Repository<AspNetUsers>().Update(instructor);
+
+                        //            try
+                        //            {
+                        //                await _unitOfWork.Save();
+                        //                successCount++;
+                        //            }
+                        //            catch (Exception ex)
+                        //            {
+                        //                Console.WriteLine(ex);
+                        //                errorCount++;
+                        //            }
+
+                        //        }
+                        //    }
+                        //    else
+                        //    {
+                        //        errorCount++;
+                        //    }
+                    }
+                }
+            }
+
+            if (successCount > 0)
+            {
+                response.Success = true;
+                response.Message = $"{successCount} Successful & {errorCount} Unsuccessful";
+            }
+            else
+            {
+                response.Success = false;
+                response.Message = $"{successCount} Successful & {errorCount} Unsuccessful";
+            }
+
+            return response;
+
+        }
+    }
+}
---
+++ SchoolManagement.Application/Features/TraineeBIODataGeneralInfos/Requests/Commands/UploadServiceInstructorCommand.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using SchoolManagement.Application.Responses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolManagement.Application.Features.TraineeBIODataGeneralInfos.Requests.Commands
+{
+    public class UploadServiceInstructorCommand : IRequest<BaseCommandResponse>
+    {
+        public IFormFile ServiceInstructorFile { get; set; }
+        public string BranchId { get; set; }
+    }
+}
