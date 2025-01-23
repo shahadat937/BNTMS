dc532f126dd71270d190e3a77dae7bca0d08cee1
infinityaziz <abdulaziz@infinitytechltd.com>
2025-01-23 10:48:31 +0600

    add marklist-interservice-foreign-course component

---
+++ ClientApp/src/app/admin/dashboard/marklist-interservice-foreign-course/marklist-interservice-foreign-course.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-marklist-interservice-foreign-course',
+  templateUrl: './marklist-interservice-foreign-course.component.html',
+  styleUrls: ['./marklist-interservice-foreign-course.component.sass']
+})
+export class MarklistInterserviceForeignCourseComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
